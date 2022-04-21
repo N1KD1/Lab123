3deca14914a014943fbfe4092a671f73d683f0c0
N1KD1 <panama200403@gmail.com>
2022-04-21 19:02:24 +0300

    Working show func

--- Lab123/Lab123.cpp
+++ Lab123/Lab123.cpp
@@ -4,5 +4,7 @@
 int main()
 {
 	constexpr int arr_size = 30;
-
+	student all_students[arr_size];
+	fill_blank(all_students, arr_size);
+	show_array(all_students, arr_size);
 }
--- Lab123/student_array.cpp
+++ Lab123/student_array.cpp
@@ -1,28 +1,54 @@
 #include "student_array.h"
+
+#include <iomanip>
+#include <iostream>
 #include <string>
 
 using namespace std;	
 
-void show_array(student* studs, int size)
+void show_array(const student *studs, int size)
+{
+	cout <<setw(3) <<left << "#" << setw(10) << "SURNAME" << setw(10) << "GROUP" << setw(6) <<left<< "MARK" << setw(7) << right<< "STATUS\n";
+	for(int i =0;i<size;i++)
+	{
+		cout << setw(3) << left << i + 1 << setw(10) << studs[i].surname << setw(10) << studs[i].group << setw(6)<<left << studs[i].mark << setw(7)<<right;
+		if(!studs[i].status)
+		{
+			cout << "Contract\n";
+		}
+		else
+		{
+			cout << "Budget\n";
+		}
+	}
+}
+
+
+void shaker_sort(student *studs, int size)
 {
 }
 
-void shaker_sort(student* studs, int size)
+void group_count(student *studs, int size)
 {
 }
 
-void group_count(student* studs, int size)
+void bubble_sort(student *studs, int size)
 {
 }
 
-void bubble_sort(student* studs, int size)
+void index_search(student *studs, int size)
 {
 }
 
-void index_search(student* studs, int size)
+void rnd_fill(student *studs, int size)
 {
 }
 
-void rnd_fill(student* studs, int size)
+void fill_blank(student *studs, int size)
 {
+	student temp = {"aaaa","bbbb",100,true};
+	for(int i = 0;i<size;i++)
+	{
+		studs[i] = temp;
+	}
 }
